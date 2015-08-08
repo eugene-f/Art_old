358bb59222955a70989ebbeaf78b49133ca2e6b7
Yevgeniy Frolov <eugene.frolov.95@gmail.com>
2015-08-08 14:51:24 +0600

    Fix last commit

---
+++ src/kz/ef/art/Utils.java
@@ -0,0 +1,23 @@
+package kz.ef.art;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Utils {
+
+    public static final Random random = new Random();
+
+    public static void printFrameSize(JFrame frame) {
+        final Dimension formSize = frame.getSize();
+        final Dimension paneSize = frame.getContentPane().getSize();
+        final double bordersWidth = formSize.getWidth() - paneSize.getWidth();
+        final double bordersHeight = formSize.getHeight() - paneSize.getHeight();
+
+        System.out.println(frame.getTitle() + " paneSize: " + paneSize);
+        System.out.println(frame.getTitle() + " formSize: " + formSize);
+        System.out.println(frame.getTitle() + " bordersWidth: " + bordersWidth);
+        System.out.println(frame.getTitle() + " bordersHeight: " + bordersHeight);
+    }
+
+}
